8d1dbb285031a45ce17c5b320881437db041be30
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-11 16:53:52 -0300

    Category entity

--- src/main/java/com/yujiyamamoto64/market7/resources/CategoryResource.java
+++ src/main/java/com/yujiyamamoto64/market7/resources/CategoryResource.java
@@ -1,15 +1,25 @@
 package com.yujiyamamoto64.market7.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yujiyamamoto64.market7.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@GetMapping
-	public String list() {
-		return "rest's working";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informatic");
+		Category cat2 = new Category(2, "Office");
+		List<Category> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1, cat2));
+		return list;
 	}
 }
