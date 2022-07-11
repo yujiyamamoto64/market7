9b86b8cf4d633cb365eb4bfe4ea707d218de206e
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-11 16:13:28 -0300

    first commit

---
+++ src/main/java/com/yujiyamamoto64/market7/Market7Application.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.market7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Market7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Market7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/market7/Market7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.market7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Market7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
