b93136674cd46407329b9ef0df473e7202556a16
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-18 14:05:06 -0300

    City and State

--- src/main/java/com/yujiyamamoto64/market7/Market7Application.java
+++ src/main/java/com/yujiyamamoto64/market7/Market7Application.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.yujiyamamoto64.market7.domain.Category;
+import com.yujiyamamoto64.market7.domain.City;
 import com.yujiyamamoto64.market7.domain.Product;
+import com.yujiyamamoto64.market7.domain.State;
 import com.yujiyamamoto64.market7.repositories.CategoryRepository;
+import com.yujiyamamoto64.market7.repositories.CityRepository;
 import com.yujiyamamoto64.market7.repositories.ProductRepository;
+import com.yujiyamamoto64.market7.repositories.StateRepository;
 
 @SpringBootApplication
 public class Market7Application implements CommandLineRunner{
@@ -20,6 +24,12 @@ public class Market7Application implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Market7Application.class, args);
@@ -43,6 +53,19 @@ public class Market7Application implements CommandLineRunner{
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		State st1 = new State(null, "Sao Paulo");
+		State st2 = new State(null, "Minas Gerais");
+		
+		City c1 = new City(null, "Uberlandia", st1);
+		City c2 = new City(null, "Sao Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+		
+		st1.getCities().add(c1);
+		st2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/yujiyamamoto64/market7/domain/City.java
@@ -0,0 +1,77 @@
+package com.yujiyamamoto64.market7.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class City implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "state_id")
+	private State states;
+	
+	public City() {
+		
+	}
+
+	public City(Integer id, String name, State states) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.states = states;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public State getStates() {
+		return states;
+	}
+
+	public void setStates(State states) {
+		this.states = states;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		return Objects.equals(id, other.id);
+	}
+}
