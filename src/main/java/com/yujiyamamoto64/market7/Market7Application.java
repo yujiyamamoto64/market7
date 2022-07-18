627cd1dd97f7a7cfc30637cebdfeedc4c47d5497
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-18 10:47:24 -0300

    Adding some Category instances

--- src/main/java/com/yujiyamamoto64/market7/Market7Application.java
+++ src/main/java/com/yujiyamamoto64/market7/Market7Application.java
@@ -1,13 +1,31 @@
 package com.yujiyamamoto64.market7;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Market7Application {
+import com.yujiyamamoto64.market7.domain.Category;
+import com.yujiyamamoto64.market7.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class Market7Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Market7Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informatic");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
