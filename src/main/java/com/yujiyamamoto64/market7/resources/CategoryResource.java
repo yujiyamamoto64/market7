8a6847344846069c1ef40befa079533e32f38809
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-20 18:02:30 -0300

    Category findAll method with DTO

--- src/main/java/com/yujiyamamoto64/market7/resources/CategoryResource.java
+++ src/main/java/com/yujiyamamoto64/market7/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.yujiyamamoto64.market7.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.yujiyamamoto64.market7.domain.Category;
+import com.yujiyamamoto64.market7.dto.CategoryDTO;
 import com.yujiyamamoto64.market7.services.CategoryService;
 
 @RestController
@@ -53,5 +56,14 @@ public class CategoryResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream()
+				.map(obj -> new CategoryDTO(obj))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/yujiyamamoto64/market7/services/CategoryService.java
+++ src/main/java/com/yujiyamamoto64/market7/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.yujiyamamoto64.market7.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoryService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Cant exclude category with products associated.");
 		}
+	}
+
+	public List<Category> findAll() {
+		return repo.findAll();
 	}
 }
